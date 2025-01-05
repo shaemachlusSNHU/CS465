3956dd6e05ad084bd6b04ac6e2ca4d992ce84f3a
Shae Machlus <shaemachlus@gmail.com>
2025-01-05 13:41:08 -0500

    Module 5 completed baseline

--- app_api/controllers/trips.js
+++ app_api/controllers/trips.js
@@ -22,8 +22,7 @@ const tripsList = async (req, res) => {
 // GET: /trips/:tripCode - lists a single trip
 // Regardless of outcome, response must include HTML status code and JSON message to the requesting client
 const tripsFindByCode = async (req, res) => {
-  const q = await Model
-    .find({'code' : req.params.tripCode }) // Return single record
+  const q = await Model.find({ code: req.params.tripCode }) // Return single record
     .exec();
 
   // Uncomment the following line to show results of query on the console
@@ -39,5 +38,5 @@ const tripsFindByCode = async (req, res) => {
 
 module.exports = {
   tripsList,
-  tripsFindByCode
+  tripsFindByCode,
 };
--- app_api/routes/index.js
+++ app_api/routes/index.js
@@ -5,8 +5,8 @@ const router = express.Router(); // Router logic
 const tripsController = require("../controllers/trips");
 
 // define route for our trips endpoint
-router.route("trips").get(tripsController.tripsList); // GET Method routes tripList
+router.route("/trips").get(tripsController.tripsList); // GET Method routes tripList
 // GET Method routes tripsFindByCode - requires parameter
-router.route("trips/:tripCode").get(tripsController.tripsFindByCode);
+router.route("/trips/:tripCode").get(tripsController.tripsFindByCode);
 
 module.exports = router;
